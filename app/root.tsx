5877881fc25a30d806ca976fe8702a8a480814d6
yurika watanabe <yurika-watanabe@nekokari.co.jp>
2025-04-25 09:43:04 +0900

    feat: エラーハンドリングと共通リンクの定義を追加

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,9 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+/**
+ * 全ページ共通の link を定義
+ */
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -23,40 +26,61 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+/**
+ * 全ページ共通のHTMLを定義するコンポーネント
+ * @memo 全ページに適用されるため、特定のページに依存しない内容を記述する
+ */
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* すべてのルートのすべての `meta` export はここにレンダリングされる */}
         <Meta />
+        {/* すべてのルートのすべての `link` export はここにレンダリングされる */}
         <Links />
       </head>
       <body>
+        {/* 子ルートレンダリング */}
         {children}
+        {/* クライアント側の遷移のスクロール位置を管理 */}
         <ScrollRestoration />
+        {/* スクリプトタグ */}
         <Scripts />
       </body>
     </html>
   );
 }
 
+/**
+ * アプリケーションのルートコンポーネント
+ * @description Layout の children に入る要素。全ページ共通の表示を定義
+ */
 export default function App() {
   return <Outlet />;
 }
 
+/**
+ * エラーハンドリング
+ * @description アプリケーション内で発生したエラーをキャッチし、ユーザーに適切なエラーメッセージを表示
+ * @param param.error
+ * @returns
+ */
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
+  let message = "エラーが発生しました";
+  let details = "予期せぬエラーが発生しました";
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
+    // ルートエラーの場合
     message = error.status === 404 ? "404" : "Error";
     details =
       error.status === 404
-        ? "The requested page could not be found."
+        ? "ページが見つかりません"
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
+    // 開発環境でのエラーの場合
     details = error.message;
     stack = error.stack;
   }
